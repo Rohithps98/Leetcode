ca92441ec4d6548a1ee229e5d4ab9622f5954c68
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-16 11:39:31 -0700

    Time: 86 ms (25.72%), Space: 17 MB (11.09%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,16 +1,21 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        def is_good(nums):
+        def is_good(nums,dp):
+            if nums in dp:
+                return dp[nums]
             rotated = {'1':'1','2':'5','5':'2','6':'9','8':'8','9':'6','0':'0'}
             rotated_num = ""
             for digit in str(nums):
                 if digit in ['3','4','7']:
+                    dp[nums] = False
                     return False
                 else:
                     rotated_num+=rotated[digit]
-            return rotated_num!=str(nums)
+            dp[nums] = rotated_num!=str(nums)
+            return dp[nums]
         count= 0
+        dp = {}
         for i in range(1,n+1):
-            if is_good(i):
+            if is_good(i,dp):
                 count+=1
         return count
