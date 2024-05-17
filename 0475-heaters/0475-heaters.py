184d4b6da37e8e46e9dba52d8217a3589a819146
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-17 10:58:14 -0700

    Time: 294 ms (38.68%), Space: 20.4 MB (9.88%) - LeetHub

--- 0475-heaters/0475-heaters.py
+++ 0475-heaters/0475-heaters.py
@@ -2,28 +2,28 @@ class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         houses.sort()
         heaters.sort()
-        max_radius = 0
+        maxradius = 0
         for house in houses:
             leftheater = self.findclosestheater(heaters,house)
             rightheater = leftheater+1
             if leftheater>=0:
                 distanceleft = abs(house-heaters[leftheater])
             else:
-                distanceleft = float(inf)
+                distanceleft = float('inf')
             if rightheater<len(heaters):
                 distanceright = abs(house-heaters[rightheater])
             else:
-                distanceright = float(inf)
-            max_radius = max(max_radius,min(distanceleft,distanceright))
-        return max_radius
-    def findclosestheater(self,heaters,target):
+                distanceright = float('inf')
+            maxradius = max(maxradius,min(distanceleft,distanceright))
+        return maxradius
+    def findclosestheater(self,heaters,house):
         left,right = 0,len(heaters)-1
         while left<=right:
             mid = (left+right)//2
-            if heaters[mid]==target:
+            if heaters[mid]==house:
                 return mid
-            elif heaters[mid]<target:
-                left=mid+1
-            else:
-                right=mid-1
+            elif heaters[mid]<house:
+                left = mid+1
+            elif heaters[mid]>house:
+                right = mid-1
         return right
