7a3e885c5dd477f3df8a3ed13d1569dff5673a0a
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-21 10:35:08 -0700

    Time: 29 ms (89.26%), Space: 16.7 MB (13.63%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,10 +1,6 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
         a = collections.Counter(s)
-        if 'A' in a and a['A']>=2:
+        if a['A']>=2 or 'LLL' in s:
             return False
-        for i in range(len(s)-2):
-            if s[i]=='L':
-                if s[i+1]==s[i+2]=='L':
-                    return False
         return True
