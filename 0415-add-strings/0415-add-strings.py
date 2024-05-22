19a1e91aa3773b968f185488f71f8f48a049a34f
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-22 13:48:27 -0700

    Time: 49 ms (19.44%), Space: 16.6 MB (83.49%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,11 +1,11 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         i,j,carry,result = len(num1)-1,len(num2)-1,0,[]
-        def chartoint(ch):
+        def chrtoint(ch):
             return ord(ch)-ord('0')
         while i>=0 or j>=0 or carry:
-            n1=chartoint(num1[i]) if i>=0 else 0
-            n2 = chartoint(num2[j]) if j>=0 else 0
+            n1 = chrtoint(num1[i]) if i>=0 else 0
+            n2 = chrtoint(num2[j]) if j>=0 else 0
             total = n1+n2+carry
             carry = total//10
             result.append(chr(total%10+ord('0')))
