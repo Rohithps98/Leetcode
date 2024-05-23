3f9a18f6e196045f482dad384e4010f82b22fd62
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-23 07:45:23 -0700

    Time: 133 ms (25.92%), Space: 17.8 MB (31.20%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        pre = 0
+        su = sum(nums)
+        for i in range(len(nums)):
+            if (pre*2)!=su-nums[i]:
+                pre+=nums[i]
+                print(pre)
+            else:
+                return i
+        return -1
