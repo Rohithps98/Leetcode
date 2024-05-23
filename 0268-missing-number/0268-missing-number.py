8a7c437ffee9f36115f62ec22a87b3ebba6ce06a
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-23 15:43:05 -0700

    Time: 114 ms (46.00%), Space: 18.5 MB (7.11%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,7 +4,12 @@ class Solution:
         # asu = (n*(n+1))//2
         # cs = sum(nums)
         # return asu-cs
-        miss = len(nums)
-        for i,num in enumerate(nums):
-            miss ^= i^num
-        return miss
+        # miss = len(nums)
+        # for i,num in enumerate(nums):
+        #     miss ^= i^num
+        # return miss
+        a = set(nums)
+        n = len(nums)
+        for i in range(n+1):
+            if i not in a:
+                return i
