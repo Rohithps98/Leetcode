60521cfe8531246165f4db8976aabd4058f5f5fa
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-24 06:16:36 -0700

    Time: 108 ms (48.41%), Space: 17.7 MB (34.03%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,11 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.eow = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self,word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.eow = True
+    def dfs(self):
+        stack = [(self.root,'')]
+        longest = ''
+        while stack:
+            node,path = stack.pop()
+            if node.eow:
+                if len(path)>len(longest):
+                    longest = path
+                elif len(path)==len(longest) and path<longest:
+                    longest = path
+            for char,child in node.children.items():
+                if child.eow:
+                    stack.append((child,path+char))
+        return longest
+                
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        words.sort()
-        wordset = set([''])
-        longest = ''
+        trie = Trie()
         for word in words:
-            if word[:-1] in wordset:
-                wordset.add(word)
-                if len(word)>len(longest):
-                    longest = word
-        return longest
+            trie.insert(word)
+        return trie.dfs()
