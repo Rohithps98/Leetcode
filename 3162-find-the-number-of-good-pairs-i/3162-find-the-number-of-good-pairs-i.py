a0ac59dff148cab1f52058933ab14e72bba3e77b
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-05-30 12:50:22 -0700

    Time: 47 ms (87.20%), Space: 16.6 MB (61.03%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.py
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.py
@@ -1,8 +1,18 @@
+from collections import defaultdict
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        a = 0
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i]%(nums2[j]*k)==0:
-                    a+=1
-        return a
+        countmap = defaultdict(int)
+        for num in nums2:
+            countmap[num*k]+=1
+        c = 0
+        for i in nums1:
+            for d in countmap:
+                if i%d==0:
+                    c+=countmap[d]
+        return c
+        # a = 0
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i]%(nums2[j]*k)==0:
+        #             a+=1
+        # return a
