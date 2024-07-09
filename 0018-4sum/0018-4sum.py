67778235b93d0f421406e5294e0ce58a23f32d02
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-07-08 17:09:00 -0700

    Time: 456 ms (54.33%), Space: 16.6 MB (27.10%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,20 +1,27 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums = sorted(nums)
-        a = []
-        for i in range(len(nums)-3):
-            for j in range(i+1,len(nums)-2):
-                lo = j+1
-                hi = len(nums)-1
-                while lo<hi:
-                    # cu = nums[i]+nums[j]+nums[left]+nums[right]
-                    if nums[lo]+nums[hi]+nums[i]+nums[j] == target and sorted([nums[i],nums[j],nums[lo],nums[hi]]) not in a:
-                        a.append([nums[i],nums[j],nums[lo],nums[hi]])
-                        lo+=1
-                        hi-=1
-                    elif nums[lo]+nums[hi]+nums[i]+nums[j] > target:
-                        hi-=1
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,n-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                left,right = j+1,n-1
+                while left<right:
+                    total = nums[i]+nums[j]+nums[left]+nums[right]
+                    if total == target:
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        while left<right and nums[left]==nums[left+1]:
+                            left+=1
+                        while left<right and nums[right]==nums[right-1]:
+                            right-=1
+                        left+=1
+                        right-=1
+                    elif total<target:
+                        left+=1
                     else:
-                        lo+=1
-
-        return a
+                        right-=1
+        return res
