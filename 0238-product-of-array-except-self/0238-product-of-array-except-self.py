3f02720d503328d293548e4a58130c9619e51746
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-08-12 16:41:06 -0700

    Time: 268 ms (59.29%), Space: 25.7 MB (47.34%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,10 @@
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#         res = [1]*len(nums)
-#         # res = []
-#         prefix = 1
-#         for i in range(len(nums)):
-#         # for i in nums:
-#             res[i] = prefix
-#             prefix*=i
-#         postfix = 1
-#         for i in range(len(nums)-1,-1,-1):
-#             res[i]*=postfix
-#             postfix*=nums[i]
-#         return res
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1]*len(nums)
+        # res = []
         prefix = 1
         for i in range(len(nums)):
+        # for i in nums:
             res[i] = prefix
             prefix*=nums[i]
         postfix = 1
@@ -24,3 +12,15 @@ class Solution:
             res[i]*=postfix
             postfix*=nums[i]
         return res
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         res = [1]*len(nums)
+#         prefix = 1
+#         for i in range(len(nums)):
+#             res[i] = prefix
+#             prefix*=nums[i]
+#         postfix = 1
+#         for i in range(len(nums)-1,-1,-1):
+#             res[i]*=postfix
+#             postfix*=nums[i]
+#         return res
