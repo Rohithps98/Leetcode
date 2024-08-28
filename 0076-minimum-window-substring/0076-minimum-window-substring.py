515450a3a61422582feb3d183cb604f622b30dd2
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-08-28 16:05:41 -0700

    Time: 90 ms (93.41%), Space: 17.2 MB (75.83%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == '':
+            return ''
+        countt,window = {},{}
+        for i in t:
+            countt[i] = 1+countt.get(i,0)
+        l,have,need = 0,0,len(countt)
+        res,resl = [-1,-1],float('inf')
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1+window.get(c,0)
+            if c in countt and countt[c]==window[c]:
+                have+=1
+            while have==need:
+                if resl>r-l+1:
+                    res = [l,r]
+                    resl = r-l+1
+                window[s[l]]-=1
+                if s[l] in countt and window[s[l]]<countt[s[l]]:
+                    have-=1
+                l+=1
+        l,r = res
+        return s[l:r+1] if resl!=float('inf') else ""
