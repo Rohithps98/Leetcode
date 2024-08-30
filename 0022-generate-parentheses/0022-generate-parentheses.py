fa038e8d2446887b8ce09231a84b8ff1a68a46ff
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-08-30 10:57:45 -0700

    Time: 40 ms (40.29%), Space: 16.7 MB (80.73%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        # l,r = 0,0
+        def gp(s,l,r):
+            if len(s)==2*n:
+                res.append(s)
+                return
+            if l<n:
+                gp(s+'(',l+1,r)
+            if r<l:
+                gp(s+')',l,r+1)
+        gp("",0,0)
+        return res
