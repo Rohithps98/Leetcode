c1c74857567a1932535f9a32d946a9bcaa10981f
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-09-12 23:05:50 -0700

    Time: 61 ms (94.80%), Space: 16.5 MB (95.53%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        totalsum = sum(nums)
+        if totalsum<target or (totalsum+target)%2!=0:
+            return 0
+        required = (totalsum+target)//2
+        if required<0:
+            return 0
+        dp = [0]*(required+1)
+        dp[0]=1
+        for i in nums:
+            for j in range(required,i-1,-1):
+                dp[j] +=dp[j-i]
+        return dp[required]          
