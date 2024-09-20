616be91032584002be9e72d9eec2de094a76bea9
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-09-20 11:13:22 -0700

    Time: 47 ms (75.74%), Space: 16.5 MB (94.13%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -4,9 +4,9 @@ class Solution:
         target = totalsum//2
         dp = [False]*(target+1)
         dp[0] = True
-        for stone in stones:
-            for i in range(target,stone-1,-1):
-                dp[i] = dp[i] or dp[i-stone]
+        for i in stones:
+            for j in range(target,i-1,-1):
+                dp[j] = dp[j] or dp[j-i]
         for s in range(target,-1,-1):
             if dp[s]:
                 return totalsum-2*s
