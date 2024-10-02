5d97133e81fdffc19dd5b54b7f3d182434232e45
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-10-02 15:09:33 -0700

    Time: 209 ms (90.72%), Space: 17.8 MB (74.10%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,32 +1,8 @@
-# class Solution:
-#     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-#         if not matrix:
-#             return 0
-#         def largestrectanglearea(heights):
-#             max_area = 0
-#             st = []
-#             heights.append(0)
-#             for i in range(len(heights)):
-#                 while st and heights[i]<heights[st[-1]]:
-#                     h = heights[st.pop()]
-#                     w = i if not st else i-st[-1]-1
-#                     max_area = max(max_area,h*w)
-#                 st.append(i)
-#             heights.pop()
-#             return max_area
-#         max_area = 0
-#         n = len(matrix[0])
-#         heights = [0]*n
-#         for row in A:
-#             for i in range(n):
-#                 heights[i] = heights[i]+1 if row[i]==1 else 0
-#             max_area = max(max_area,largestrectanglearea(heights))
-#         return max_area
 class Solution:
-    def maximalRectangle(self, A):
-        if not A:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix:
             return 0
-        def largestrectanglearea(heights):
+        def largestarea(heights):
             max_area = 0
             st = []
             heights.append(0)
@@ -39,10 +15,10 @@ class Solution:
             heights.pop()
             return max_area
         max_area = 0
-        n = len(A[0])
-        heights = [0]*n
-        for row in A:
+        n = len(matrix[0])
+        heights=[0]*n
+        for row in matrix:
             for i in range(n):
-                heights[i] = heights[i]+1 if row[i]=="1" else 0
-            max_area = max(max_area,largestrectanglearea(heights))
+                heights[i] = heights[i]+1 if row[i]=='1' else 0
+            max_area = max(max_area,largestarea(heights))
         return max_area
