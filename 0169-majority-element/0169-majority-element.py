0d2792fb994057325b9274a85aad5d7411f2c33f
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-10-01 17:39:04 -0700

    Time: 159 ms (95.19%), Space: 17.9 MB (82.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,12 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        a = Counter(nums)
-        for i,j in a.items():
-            if j>len(nums)/2:
-                return i
+        candidate = None
+        count = 0
+        for num in nums:
+            if count==0:
+                candidate = num
+            if num==candidate:
+                count+=1
+            else:
+                count-=1
+        return candidate
