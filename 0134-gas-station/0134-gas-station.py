7f4adf96375cf6f3dbc1cc26474a799e8bf46260
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-10-01 17:28:45 -0700

    Time: 760 ms (97.30%), Space: 22 MB (37.69%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,5 +1,14 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas)<sum(cost):
+            return -1
+        total,res = 0,0
+        for i in range(len(gas)):
+            total+=gas[i]-cost[i]
+            if total<0:
+                res = i+1
+                total = 0
+        return res
         # if sum(gas)<sum(cost):
         #     return -1
         # total,res = 0,0
@@ -9,19 +18,19 @@ class Solution:
         #         total = 0
         #         res = i+1
         # return res
-        n = len(gas)
-        total_gas = 0
-        total_cost = 0
-        current_gas = 0
-        start_index = 0
-        for i in range(n):
-            total_gas += gas[i]
-            total_cost += cost[i]
-            current_gas += gas[i]-cost[i]
+#         n = len(gas)
+#         total_gas = 0
+#         total_cost = 0
+#         current_gas = 0
+#         start_index = 0
+#         for i in range(n):
+#             total_gas += gas[i]
+#             total_cost += cost[i]
+#             current_gas += gas[i]-cost[i]
             
-            if current_gas<0:
-                start_index = i+1
-                current_gas = 0
-        if total_gas<total_cost:
-            return -1
-        return start_index
+#             if current_gas<0:
+#                 start_index = i+1
+#                 current_gas = 0
+#         if total_gas<total_cost:
+#             return -1
+#         return start_index
