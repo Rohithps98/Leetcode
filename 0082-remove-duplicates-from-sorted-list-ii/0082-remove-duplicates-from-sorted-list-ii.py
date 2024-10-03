6dbeca0262f6bceeb6e98ca7aa6d2552c05d2722
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-10-03 13:44:24 -0700

    Time: 45 ms (19.85%), Space: 16.5 MB (43.01%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -10,7 +10,7 @@ class Solution:
         prev = dummy
         while head:
             if head.next and head.val == head.next.val:
-                while head.next and head.val == head.next.val:
+                while head.next and head.val==head.next.val:
                     head = head.next
                 prev.next = head.next
             else:
