cba8f38317617159998c34d1d33924bd815d724c
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2024-12-10 16:36:13 -0800

    Time: 0 ms (100.00%), Space: 17.3 MB (10.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one = 2
-        two = 1
+        # one = 2
+        # two = 1
+        # if n==1 or n==2:
+        #     return n
+        # for i in range(3,n+1):
+        #     cur = one+two
+        #     two = one
+        #     one = cur
+        # return one
         if n==1 or n==2:
             return n
-        for i in range(3,n+1):
-            cur = one+two
-            two = one
-            one = cur
-        return one
+        dp = [0]*n
+        dp[0],dp[1] = 1,2
+        for i in range(2,n):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[-1]
