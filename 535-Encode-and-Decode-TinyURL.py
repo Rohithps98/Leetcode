7d2cfc53eb6b8dffc8297b00a2bc69e5414045d5
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2025-02-10 10:03:22 -0800

    [535] [Time Beats: 88.83%] [Memory Beats: 20.15%] - LeetPush

--- 535-Encode-and-Decode-TinyURL.py
+++ 535-Encode-and-Decode-TinyURL.py
@@ -5,25 +5,26 @@ class Codec:
         self.shortmap = {}
         self.urlmap = {}
         self.base = 'https://tinyurl.com'
-    def shortkey(self):
+    def genkey(self):
         return ''.join(random.choices(string.ascii_letters+string.digits,k=6))
+
     def encode(self, longUrl: str) -> str:
         \\\Encodes a URL to a shortened URL.
         \\\
-        if longUrl in self.shortmap:
+        while longUrl in self.shortmap:
             return self.shortmap[longUrl]
-        short_key = self.shortkey()
-        while short_key in self.urlmap:
-            short_key = self.shortkey()
-        short_url = self.base+short_key
-        self.urlmap[short_url] = longUrl
-        self.shortmap[longUrl] = short_url
-        return short_url
+        shortkey = self.genkey()
+        while shortkey in self.urlmap:
+            shortkey = self.genkey()
+        shorturl = self.base+shortkey
+        self.urlmap[shorturl] = longUrl
+        self.shortmap[longUrl] = shorturl
+        return shorturl
 
     def decode(self, shortUrl: str) -> str:
         \\\Decodes a shortened URL to its original URL.
         \\\
-        return self.urlmap.get(shortUrl,\\)
+        return self.urlmap.get(shortUrl,'')
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
