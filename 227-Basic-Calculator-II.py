6353a1ea401679550808d148f2f4fec818fcb950
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2025-02-11 23:56:49 -0800

    [227] [Time Beats: 74.20%] [Memory Beats: 46.19%] - LeetPush

--- 227-Basic-Calculator-II.py
+++ 227-Basic-Calculator-II.py
@@ -1,25 +1,21 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s.replace(\ \, \\)  # Remove spaces
-        stack = []
+        s = s.replace(\ \,\\)
+        st = []
         num = 0
-        prev_op = '+'
-        
-        for i, char in enumerate(s):
+        prevop = '+'
+        for i,char in enumerate(s):
             if char.isdigit():
-                num = num * 10 + int(char)  # Build the current number
-            
-            if char in \+-*/\ or i == len(s) - 1:  # If operator or end of string
-                if prev_op == '+':
-                    stack.append(num)
-                elif prev_op == '-':
-                    stack.append(-num)
-                elif prev_op == '*':
-                    stack.append(stack.pop() * num)
-                elif prev_op == '/':
-                    stack.append(int(stack.pop() / num))  # Truncate toward zero
-                
-                prev_op = char  # Update operator
-                num = 0  # Reset number
-        
-        return sum(stack)
+                num = num*10+int(char)
+            if char in '+-*/' or i==len(s)-1:
+                if prevop == '+':
+                    st.append(num)
+                elif prevop =='-':
+                    st.append(-num)
+                elif prevop == '/':
+                    st.append(int(st.pop()/num))
+                elif prevop == '*':
+                    st.append(st.pop()*num)
+                prevop = char
+                num = 0
+        return sum(st)
