f24211d3d1929b87f6b472f126cccb4597c082b3
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2025-02-19 15:41:25 -0800

    [71] [Time Beats: 100.00%] [Memory Beats: 47.99%] - LeetPush

---
+++ 71-Simplify-Path.py
@@ -0,0 +1,10 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        st = []
+        for part in path.split('/'):
+            if part=='..':
+                if st:
+                    st.pop()
+            elif part and part!='.':
+                st.append(part)
+        return '/'+'/'.join(st)
