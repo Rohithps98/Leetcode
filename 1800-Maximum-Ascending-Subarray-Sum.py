e08f8ab66c486e366144ffc2358fd35030a1b165
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2025-02-26 09:01:19 -0800

    [1800] [Time Beats: 100.00%] [Memory Beats: 79.27%] - LeetPush

---
+++ 1800-Maximum-Ascending-Subarray-Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        maxsum = 0
+        cursum = nums[0]
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                cursum += nums[i]
+            else:
+                maxsum = max(cursum,maxsum)
+                cursum = nums[i]
+        return max(maxsum,cursum)
