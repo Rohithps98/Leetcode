8b25fcc6d4d3c4ef47a15fd42ae53ef8b4b62dad
Rohith ps <95596304+Rohithps98@users.noreply.github.com>
2025-04-05 08:52:04 -0700

    [1863] [Time Beats: 100.00%] [Memory Beats: 83.05%] - LeetPush

--- DCP-04-25/Sum-of-All-Subset-XOR-Totals.py
+++ DCP-04-25/Sum-of-All-Subset-XOR-Totals.py
@@ -1,19 +1,6 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def generate_subsets(nums, index, subset, subsets):
-            if index == len(nums):
-                subsets.append(subset[:])
-                return
-            subset.append(nums[index])
-            generate_subsets(nums, index+1, subset, subsets)
-            subset.pop()
-            generate_subsets(nums, index+1, subset, subsets)
-        subsets = []
-        generate_subsets(nums, 0, [], subsets)
         result = 0
-        for subset in subsets:
-            subset_total = 0
-            for num in subset:
-                subset_total ^= num
-            result += subset_total
-        return result
+        for num in nums:
+            result |= num
+        return result << (len(nums)-1)
